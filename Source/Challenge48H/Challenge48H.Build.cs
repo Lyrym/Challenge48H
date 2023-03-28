0205de17dd82271d645bb5b630dc64632f554dba
Titouan MATHIS <55627323+Lyrym@users.noreply.github.com>
2023-03-29 00:32:09 +0200

    ADD: Parsing Scorelist with json

--- Source/Challenge48H/Challenge48H.Build.cs
+++ Source/Challenge48H/Challenge48H.Build.cs
@@ -8,7 +8,7 @@ public class Challenge48H : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HTTP" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HTTP", "Json" });
 
         PublicDependencyModuleNames.AddRange(new string[] { "Challenge48H" });
 
