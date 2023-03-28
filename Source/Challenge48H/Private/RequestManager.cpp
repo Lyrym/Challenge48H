3ddcf59c4e733adf704c4cd2915fbb75fa81e5d8
Titouan MATHIS <mathis.tit@gmail.com>
2023-03-28 14:02:17 +0200

    ADD: API BP Requests

--- Source/Challenge48H/Private/RequestManager.cpp
+++ Source/Challenge48H/Private/RequestManager.cpp
@@ -3,42 +3,37 @@
 
 #include "RequestManager.h"
 
-RequestManager::RequestManager()
+const std::string BASE_URL = "https://lesratz.fly.dev";
+const std::string SCORE_LIST_ENDPOINT = "/scorelist";
+const std::string NEW_SCORE_ENDPOINT = "/newscore";
+
+void URequestManager::Scorelist(FOnScoreListSignature callback)
 {
-}
-
-RequestManager::~RequestManager()
-{
-}
-
-
-
-void RequestManager::Scorelist()
-{
-    std::string fullURL = BASE_URL + SCORE_LIST_ENDPOINT;
+    const std::string fullURL = BASE_URL + SCORE_LIST_ENDPOINT;
     FString url = FString(fullURL.c_str()).ToLower();
     TSharedRef<IHttpRequest, ESPMode::ThreadSafe> HttpRequest = FHttpModule::Get().CreateRequest();
     HttpRequest->SetVerb("GET");
     HttpRequest->SetURL(url);
-    HttpRequest->OnProcessRequestComplete().BindRaw(this, &RequestManager::OnScoreListReceived);
+    HttpRequest->OnProcessRequestComplete().BindLambda([callback](FHttpRequestPtr Request, FHttpResponsePtr Response, bool bSuccess)
+        {
+            if (bSuccess)
+            {
+                FString ResponseString = Response->GetContentAsString();
+                UE_LOG(LogTemp, Warning, TEXT("New score response: %s"), *ResponseString);
+                callback.ExecuteIfBound(ResponseString);
+            }
+            else
+            {
+                UE_LOG(LogTemp, Warning, TEXT("Failed to update or insert player's score."));
+                callback.ExecuteIfBound("");
+            }
+        });
+
     HttpRequest->ProcessRequest();
 
 }
 
-void RequestManager::OnScoreListReceived(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bSuccess)
-{
-    if (bSuccess)
-    {
-        FString ResponseString = Response->GetContentAsString();
-        UE_LOG(LogTemp, Warning, TEXT("Player score list response: %s"), *ResponseString);
-    }
-    else
-    {
-        UE_LOG(LogTemp, Warning, TEXT("Failed to fetch player score list."));
-    }
-}
-
-void RequestManager::NewScore(FString name, int32 score)
+void URequestManager::NewScore(FString name, int32 score, FOnNewScore callback)
 {
     std::string fullURL = BASE_URL + NEW_SCORE_ENDPOINT;
     FString url = FString(fullURL.c_str()).ToLower();
@@ -48,18 +43,19 @@ void RequestManager::NewScore(FString name, int32 score)
     HttpRequest->SetHeader(TEXT("Content-Type"), TEXT("application/json"));
     FString Content = FString::Printf(TEXT("{\"pseudo\": \"%s\", \"score\" : \"%d\"}"), *name, score);
     HttpRequest->SetContentAsString(Content);
-    HttpRequest->OnProcessRequestComplete().BindRaw(this, &RequestManager::OnNewScoreReceived);
+    HttpRequest->OnProcessRequestComplete().BindLambda([callback](FHttpRequestPtr Request, FHttpResponsePtr Response, bool bSuccess)
+        {
+            if (bSuccess)
+            {
+                FString ResponseString = Response->GetContentAsString();
+                UE_LOG(LogTemp, Warning, TEXT("New score response: %s"), *ResponseString);
+                callback.ExecuteIfBound(ResponseString);
+            }
+            else
+            {
+                UE_LOG(LogTemp, Warning, TEXT("Failed to update or insert player's score."));
+                callback.ExecuteIfBound("");
+            }
+        });
     HttpRequest->ProcessRequest();
-}
-void RequestManager::OnNewScoreReceived(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bSuccess)
-{
-    if (bSuccess)
-    {
-        FString ResponseString = Response->GetContentAsString();
-        UE_LOG(LogTemp, Warning, TEXT("New score response: %s"), *ResponseString);
-    }
-    else
-    {
-        UE_LOG(LogTemp, Warning, TEXT("Failed to update or insert player's score."));
-    }
 }
--- Source/Challenge48H/Public/RequestManager.h
+++ Source/Challenge48H/Public/RequestManager.h
@@ -5,24 +5,31 @@
 #include "HttpModule.h"
 #include "Http.h"
 #include "CoreMinimal.h"
+#include "RequestManager.generated.h"
 
 
 /**
- * 
+ *
  */
-class CHALLENGE48H_API RequestManager
+UCLASS()
+class CHALLENGE48H_API URequestManager : public UBlueprintFunctionLibrary
 {
-public:
-	RequestManager();
-	~RequestManager();
-	void Scorelist();
-	void NewScore(FString name, int32 score);
 
-private:
-	const std::string BASE_URL = "https://lesratz.fly.dev";
-	const std::string SCORE_LIST_ENDPOINT = "/scorelist";
-	const std::string NEW_SCORE_ENDPOINT = "/newscore";
-	void OnScoreListReceived(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bSuccess);
-	void OnNewScoreReceived(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bSuccess);
+	GENERATED_BODY()
+
+		DECLARE_DYNAMIC_DELEGATE_OneParam(FOnScoreListSignature, FString, ScoreList);
+
+	DECLARE_DYNAMIC_DELEGATE_OneParam(FOnNewScore, FString, Res);
+
+	UFUNCTION(BlueprintCallable, Category = "RequestsBPLibrary")
+		static void Scorelist(FOnScoreListSignature callback);
+	UFUNCTION(BlueprintCallable, Category = "RequestsBPLibrary")
+		static void NewScore(FString name, int32 score, FOnNewScore callback);
+
+	// Delegate signature
+
 
 };
+
+
+
